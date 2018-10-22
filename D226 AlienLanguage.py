791240e7e670713b9a2a0d76cb5013962a4d9cd1
sang4 <sang4@illinois.edu>
2018-10-22 17:01:54 -0400

    should come back

---
+++ D226 AlienLanguage.py
@@ -0,0 +1,10 @@
+# This problem was asked by Airbnb.
+
+# You come across a dictionary of sorted words in a language you've never seen before. 
+# Write a program that returns the correct order of letters in this language.
+
+# For example, given ['xww', 'wxyz', 'wxyw', 'ywx', 'ywz'], you should return ['x', 'z', 'w', 'y'].
+
+
+# The idea is to create a graph of characters and then find topological sorting of the created graph.
+# This is slightly above my level. I currently can't solve this 4.1 difficulty level.
