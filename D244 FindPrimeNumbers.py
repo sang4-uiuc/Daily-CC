2956fa41456d31c3ab11d81740ff8f61c8e9da94
sang4 <sang4@illinois.edu>
2018-11-20 14:32:38 -0500

    check in

--- D244 FindPrimeNumbers.py
+++ D244 FindPrimeNumbers.py
@@ -10,3 +10,22 @@
 # Implement this algorithm.
 
 # Bonus: Create a generator that produces primes indefinitely (that is, without taking N as an input).
+
+def findAllPrime(n):
+    l = [True] * 101
+    l[0] = False
+    l[1] = False
+    for i in range(2, n):
+        j = i * 2
+        while j <= 100:
+            print(j)
+            l[j] = False
+            j = j + i
+    output = []
+    for index, val in enumerate(l):
+        if val == True:
+            output.append(index)
+    print(output)
+
+findAllPrime(50)
+# n doesn't even seem to matter
